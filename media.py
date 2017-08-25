0a3aebb7a55188596728236f328f8529f0bb0362
Dave Banerjee <dvbnrg@gmail.com>
2017-08-25 12:56:55 -0500

    Updated Whitespace

--- media.py
+++ media.py
@@ -9,13 +9,12 @@ class Movie():
                     "PG-13",
                     "R"]
 
-
-    def __init__(bob, movie_title, movie_storyline, poster_image, trailer_youtube):
+    def __init__(bob, movie_title, movie_storyline,
+                 poster_image, trailer_youtube):
             bob.title = movie_title
             bob.storyline = movie_storyline
             bob.poster_image_url = poster_image
             bob.trailer_youtube_url = trailer_youtube
-
 
     def show_trailer(self):
             webbrowser.open(self.trailer_youtube_url)
